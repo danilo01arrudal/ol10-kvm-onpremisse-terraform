22e0c253c7f9cb7ebebb869a655ec9b749b46e51
Danilo Arruda <162503728+danilo01arrudal@users.noreply.github.com>
2026-09-10 09:07:03 -0300

    Update locals.tf

--- modules/vm/locals.tf
+++ modules/vm/locals.tf
@@ -1,4 +1,6 @@
 locals {
   # Nome do arquivo kickstart gerado
-  ks_filename = "anaconda-ks-${var.name}.cfg"
+  ks_filename  = "anaconda-ks-${var.name}.cfg"
+  root_size_mb = var.root_disk_size * 1024
 }
+
